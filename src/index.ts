069f0d8870d97c093086f4490885d74607e16e6e
José Silva Oliveira <jose.silva.oliveira@devoteam.com>
2022-04-06 18:05:14 +0100

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { PluginOption } from 'vite';
+import fs from 'fs';
+import path from 'path';
+
+export type I18nHotReloadConfig = {
+	folder: string
+}
+
+export default function I18nHotReload(config: I18nHotReloadConfig): PluginOption {
+	if (!fs.existsSync(path.resolve(process.cwd(), config.folder))) {
+		throw new Error(`Folder ${config.folder} doesn't exist`)
+	}
+	return {
+		name: 'i18n-hot-reload',
+		enforce: 'post',
+		// HMR
+		handleHotUpdate({ file, server }) {
+			if ( file.includes(config.folder) ) {
+				console.log('Reloading json file...');
+		
+				server.ws.send({
+					type: 'full-reload',
+
+					path: file
+				});
+			}
+		}
+	}
+}
